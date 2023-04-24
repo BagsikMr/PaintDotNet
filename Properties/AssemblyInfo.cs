1d05dfd7d10d9f6ecaf227319f27c517a694f0c6
Kacper Bagsik <bagsikmr@gmail.com>
2023-04-24 22:20:12 +0200

    rename project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LabyPT5")]
+[assembly: AssemblyTitle("MyPaint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("LabyPT5")]
+[assembly: AssemblyProduct("MyPaint")]
 [assembly: AssemblyCopyright("Copyright © HP 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
